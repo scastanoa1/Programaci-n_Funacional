32cffa74a924e0db25d87ece9dbf5fdd87c927bb
sebasca9 <scastanoa1@eafit.edu.co>
2023-04-29 19:22:18 -0500

    Se agregaron los metodos allClients, byNYCClient y byAllClient.

---
+++ src/DataSales.java
@@ -0,0 +1,4 @@
+import java.util.stream.Stream;
+public interface DataSales {
+    double sales(Stream<Venta> m);
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,2 @@
-import java.util.*;
-public class Main {
-    static DataProcessing byCity = city ->{
-        city.stream().filter(c-> c.getCity().equals("NYC")).forEach(System.out::println);
-        return city.stream().filter(c-> c.getCity().equals("NYC")) ;};
-    //static DataProcessing sales = sale -> {
-       // sale.stream().map(Venta::getSales).sum();
-    //}
+package PACKAGE_NAME;public class Main {
 }
